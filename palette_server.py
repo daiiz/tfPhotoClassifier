9cfe1186199601264771e908a6f176a12168e14a
daiiz <da.iz.mg@gmail.com>
2016-02-29 01:06:56 +0900

    flask導入

--- palette_server.py
+++ palette_server.py
@@ -1,1 +1,12 @@
 # coding: utf-8
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello ():
+    return 'Hello! This is palette_server.'
+
+
+if __name__ == '__main__':
+    app.run(port=6000)
